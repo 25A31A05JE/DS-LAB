db4a78af6cd20a35305786028a0cdb6ac9edb715
25A31A05JE <saisri.9d@gmail.com>
2026-01-28 22:43:40 +0530

    Add files via upload

---
+++ self info.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"Name: Jaya\n";
+    std::cout<<"Roll no.: 25A31A05JE\n";
+    std::cout<<"Branch: CSE\n";
+    std::cout<<"Section: F\n";
+    return 0;
+}
